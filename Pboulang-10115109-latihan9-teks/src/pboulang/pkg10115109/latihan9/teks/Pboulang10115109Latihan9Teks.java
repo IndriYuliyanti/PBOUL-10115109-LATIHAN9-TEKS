6c1088c16c0a6eaa21b821a0f3482420a64097d0
IndriYuliyanti <55839650+IndriYuliyanti@users.noreply.github.com>
2019-09-26 21:18:52 +0700

    Add files via upload

---
+++ Pboulang-10115109-latihan9-teks/src/pboulang/pkg10115109/latihan9/teks/Pboulang10115109Latihan9Teks.java
@@ -0,0 +1,27 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package pboulang.pkg10115109.latihan9.teks;
+
+/**
+ *
+ * @author User
+ */
+public class Pboulang10115109Latihan9Teks {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        
+        char c;
+        char tab = '\t';
+        char newline = '\n';
+        c = 'A';
+        System.out.println("hasil "+ newline + "char:" + tab + "berisi" + c);
+    }
+    
+}
